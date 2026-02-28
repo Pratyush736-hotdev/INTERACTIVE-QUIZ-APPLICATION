b96bf4e5b48043c9f2606ccae8c4182ac5bc6127
Pratyush736-hotdev <whypratyush736@gmail.com>
2026-02-28 13:47:30 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: ["Hyper Text Markup Language", "High Text Machine Language", "Hyperlinks Text Mark Language"],
+        correct: 0
+    },
+    {
+        question: "Which language is used for styling?",
+        answers: ["HTML", "CSS", "Python"],
+        correct: 1
+    }
+];
