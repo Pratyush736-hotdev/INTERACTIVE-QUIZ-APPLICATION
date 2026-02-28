2ad18cf02d9926329aea8451b7c205ee7c026814
Pratyush736-hotdev <whypratyush736@gmail.com>
2026-02-28 13:20:05 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,22 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        answers: ["Hyper Text Markup Language", "High Text Machine Language", "Hyper Tool Markup Language", "None"],
+        correct: 0
+    },
+    {
+        question: "Which language is used for styling web pages?",
+        answers: ["HTML", "JQuery", "CSS", "XML"],
+        correct: 2
+    },
+    {
+        question: "Which is not a JavaScript Framework?",
+        answers: ["React", "Angular", "Vue", "Django"],
+        correct: 3
+    },
+    {
+        question: "Which company developed JavaScript?",
+        answers: ["Google", "Netscape", "Microsoft", "Oracle"],
+        correct: 1
+    }
+];
